6c9311b924dd1d542f0469f035909a1f0e6e1e09
leon-do <leondo123@gmail.com>
2023-01-16 23:51:34 -0500

    comments

--- src/index.ts
+++ src/index.ts
@@ -16,10 +16,15 @@ const provider = new ethers.providers.WebSocketProvider(process.env.RPC_URL as s
 
 // listen to new blocks
 provider.on("block", async (_blockNumber) => {
+  // get block info
   const block: ethers.providers.Block = await provider.getBlock(_blockNumber);
+  // parse transactions from block info
   const transactions = block.transactions;
+  // get receipts from all transactions
   const receipts: ethers.providers.TransactionReceipt[] = await Promise.all(transactions.map((transactions) => provider.getTransactionReceipt(transactions)));
+  // parse receipts
   const events: Event[] = parseReceipts(receipts);
+  // log events
   for (const event of events) {
     console.log(event);
   }
